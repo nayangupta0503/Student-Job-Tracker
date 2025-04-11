aa08ac03aa464dc3cecee802c5929ef90b2fdadf
nayangupta0503 <nayangupta170@gmail.com>
2025-04-11 16:28:00 +0530

    Fix parameter name in checkDuplicate function for clarity

--- frontend/src/module/checkDuplicate.js
+++ frontend/src/module/checkDuplicate.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const checkDuplicate = async(name, role) => {
+const checkDuplicate = async(company, role) => {
     try {
         const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/applications`)
         const applications = Object.values(res.data);
         console.log(applications);
-        return applications.some((application) => application.company === name && application.role === role);
+        return applications.some((application) => application.company === company && application.role === role);
     } catch (error) {
         console.error("Error fetching applications:", error);
         alert("Failed to fetch applications. Please try again.");
